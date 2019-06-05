5044ae870e7caa89e457dabb9df6e05d28d14834
hinoe <orange_2646@outlook.jp>
2019-06-05 11:37:07 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 石井 美記夫 on 2019/06/05.
+//  Copyright © 2019 hinoe2646. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
